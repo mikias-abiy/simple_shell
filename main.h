91f74e032485ece84298c6f710f09ae9d756f0be
Mikias <mikias_abiy@proton.me>
2023-07-12 08:09:11 -0400

    add: main.h: The main header file for the project

---
+++ main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#define _GNU_SOURCE /* feature_test_macros(7) */
+
+/* Standard and System header files */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <errno.h>
+
+/* Local header files */
+#include "lstring.h"
+#include "lenv.h"
+#include "lmemory.h"
+
+#endif
